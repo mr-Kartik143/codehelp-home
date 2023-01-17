60a7480f8397416203aeefa3bba5ea5810a3fd9f
kartik <mrkartikauti@gmail.com>
2023-01-17 13:28:14 +0530

    commit one

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="wraper">
+      <div className="reactangle-23">
+        <img className="img1" src="logo_1_2.png" />
+        <span className="teamtxt">Team</span>
+        <span className="abouttxt">About Us</span>
+        <span className="coursestxt">Courses</span>
+        <span className="contacttxt">Contact</span>
+        <div className="startbtn"></div>
+        <div className="starttxt">Start a Project</div>
+        <span className="heading1">Team CodeHelp</span>
+        <p className="para1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          repellendus perspiciatis officia in officiis. Voluptates assumenda
+          commodi deserunt, asperiores vitae quos fugit earum distinctio laborum
+          rem saepe facere voluptatibus numquam.. <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          repellendus pyior.
+        </p>
+        <img className="img2" src="babaarji2.png" />
+
+        <span className="heading2">Founder, CEO</span>
+        <p className="para2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          repellendus perspiciatis officia in officiis. Voluptates assumenda
+          commodi deserunt, asperiores <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          repellendus perspiciatis officia in officiis. Voluptates assumenda
+          commodi deserunt, asperiores.
+        </p>
+        <img className="img3" src="babbarji.png" />
+      </div>
+      <div className="secrec">
+        <img className="img4" src="logo_1_2.png" />
+        <p className="menu1">Address</p>
+        <p className="menu2">Menu</p>
+        <p className="addtxt">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has{" "}
+        </p>
+        <div className="mlist">Team About Us Courses Contact</div>
+        <p className="ptxt">OUR Privacy Policy</p>
+        <p className="ctxt">© lorem Ipsum 2023 All Rights Reserved.</p>
+        <p className="etxt">Email</p>
+        <p className="eetxt">example@codehelp.com</p>
+
+        <div className="ov1">
+          <img className="ovp1" src="facebook.png" />
+        </div>
+        <div className="ov2">
+          <img className="ovp1" src="facebook.png" />
+        </div>
+        <div className="ov3">
+          <img className="ovp1" src="instagram.png" />
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
